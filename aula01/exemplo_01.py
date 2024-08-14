a79c63f8cb5a4c5c8031eeb3f6f4453068689dc0
osvaldo <osvaldobone16@gmail.com>
2024-08-14 10:52:52 -0300

    commit

--- aula01/exemplo_01.py
+++ aula01/exemplo_01.py
@@ -1,4 +1,4 @@
-#Criar um programa onde o usuário digite dois valores e apareça a soma 
+#Criar um programa onde o usuário digite dois valores e apareça a soma.
 print("Bom dia, "+(input("Digite seu nome: ") + "!"))
 
 print("A soma dos valores e: "+str(int(input("Digite um valor: "))+int(input("Digite outro valor: "))))
