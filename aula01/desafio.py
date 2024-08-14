5699e587b5837c829e4da4e69f9502a50bc74488
osvaldo <osvaldobone16@gmail.com>
2024-08-14 10:47:33 -0300

    alteração

---
+++ aula01/desafio.py
@@ -0,0 +1,11 @@
+#Escreva um programa em Python que solicita ao usuário para digitar seu nome, o valor do seu salário mensal 
+# e o valor do bônus que recebeu. O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome 
+# e informando o valor do salário em comparação com o bônus recebido.
+CONSTANTE_BONUS = 1000
+nome = input("digite seu nome: ")
+salario = input("digite seu salario: ")
+bonus = input("digite seu bonus: ")
+
+bonuskpi = CONSTANTE_BONUS + (float(salario) * float(bonus))
+
+print(f"Bom dia,{nome},! O cálculo do KPI do bônus de 2024 é de: {bonuskpi} reais.")
---
+++ aula01/exemplo_02.py
@@ -0,0 +1,6 @@
+nome = input("digite seu nome: ")
+print("Bom dia,", nome, "!")
+
+idade = input("digite sua idade: ")
+print("Sua idade e:", idade, "anos")    
+
