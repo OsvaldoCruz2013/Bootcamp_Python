b60530577add89b53068d8973d745bb5cde21a67
osvaldo <osvaldobone16@gmail.com>
2024-08-14 10:50:17 -0300

    v1

--- aula01/exemplo_00.py
+++ aula01/exemplo_00.py
@@ -1,10 +1,4 @@
-#print("Hello, World!")
+print("Hello, World!")
 
-#print("Olá,",(input("Digite seu nome: ") + "!"))
+print("Olá,",(input("Digite seu nome: ") + "!"))
 
-#Criar um programa onde o usuário digite dois valores e apareça a soma 
-print("Bom dia, "+(input("Digite seu nome: ") + "!"))
-
-   
-
-print("A soma dos valores e: "+str(int(input("Digite um valor: "))+int(input("Digite outro valor: "))))
