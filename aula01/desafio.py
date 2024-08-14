2b2a24cffca307c98188e416bd0aaaf391c88357
osvaldo <osvaldobone16@gmail.com>
2024-08-14 10:54:29 -0300

    desafio

--- aula01/desafio.py
+++ aula01/desafio.py
@@ -1,6 +1,6 @@
 #Escreva um programa em Python que solicita ao usuário para digitar seu nome, o valor do seu salário mensal 
 # e o valor do bônus que recebeu. O programa deve, então, imprimir uma mensagem saudando o usuário pelo nome 
-# e informando o valor do salário em comparação com o bônus recebido.
+# e informando o valor do salário em comparação com o bônus recebido
 CONSTANTE_BONUS = 1000
 nome = input("digite seu nome: ")
 salario = input("digite seu salario: ")
